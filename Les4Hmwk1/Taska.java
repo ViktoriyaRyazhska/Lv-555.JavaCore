904a199d8fee1ec4c0a8662ff101bc51f24bf04a
Ostap Mykhailivkiy <djtomsawyer1@gmail.com>
2020-12-07 14:05:15 +0200

    Dogs Enum

--- Les4Hmwk1/Taska.java
+++ Les4Hmwk1/Taska.java
@@ -1,25 +1,34 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Taska {
     public static void main(String[] args) {
-        int maxValue = Integer.MIN_VALUE;
-        int minValue = Integer.MAX_VALUE;
         int[] arr = new int[3];
         int i = 0;
         Scanner scanner = new Scanner(System.in);
         for (i = 0; i < arr.length; i++) {
             System.out.println("Type number: ");
             arr[i] = scanner.nextInt();
+        }
+    }
+    public int maxValue(int[] arr) {
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxValue) {
                 maxValue = arr[i];
             }
-            if (arr[i] < minValue) {
-                minValue = arr[i];
-            }
         }
-        System.out.println("Min Value is: " + maxValue);
-        System.out.println("Max Value is: " + minValue);
+        return maxValue;
     }
-}
+        public int minValue ( int[] arr){
+            int minValue = Integer.MAX_VALUE;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < minValue) {
+                    minValue = arr[i];
+                }
+            }
+            return minValue;
+        }
+    }
+
+
 
