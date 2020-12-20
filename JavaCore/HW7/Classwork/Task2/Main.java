6b8f336266094eda9127d5cf9ce1c3f036431b64
Andrew Kobryn <andrzjha@gmail.com>
2020-12-20 15:07:50 +0200

    change method findID

--- JavaCore/HW7/Classwork/Task2/Main.java
+++ JavaCore/HW7/Classwork/Task2/Main.java
@@ -1,36 +1,37 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer,String> employeeMap = new HashMap<>();
-        employeeMap.put(1,"A");
-        employeeMap.put(2,"B");
-        employeeMap.put(3,"C");
-        employeeMap.put(4,"D");
-        employeeMap.put(5,"E");
-        employeeMap.put(6,"F");
-        employeeMap.put(7,"G");
+        Map<Integer, String> employeeMap = new HashMap<>();
+        employeeMap.put(1, "A");
+        employeeMap.put(2, "B");
+        employeeMap.put(3, "C");
+        employeeMap.put(4, "D");
+        employeeMap.put(5, "E");
+        employeeMap.put(6, "F");
+        employeeMap.put(7, "G");
         System.out.println(employeeMap);
-        findValue(1,employeeMap);
-        findID("C",employeeMap);
-    }public static void findValue(int id, Map<Integer, String> map){
-        if(map.containsKey(id)){
+        findValue(1, employeeMap);
+        findID("C", employeeMap);
+    }
+
+    public static void findValue(int id, Map<Integer, String> map) {
+        if (map.containsKey(id)) {
             System.out.println(map.get(id));
-        }else{
+        } else {
             System.out.println("There is no " + id);
         }
-    }public static void findID(String value, Map<Integer, String> map){
-        for (Map.Entry<Integer, String> entry: map.entrySet()) {
-            if(map.containsValue(value)){
-                if(entry.getValue().equals(value)){
+    }
+
+    public static void findID(String value, Map<Integer, String> map) {
+        if (map.containsValue(value)) {
+            for (Map.Entry<Integer, String> entry : map.entrySet()) {
+                if (entry.getValue().equals(value)) {
                     System.out.println(entry.getKey());
                 }
-
-            }else {
-                System.out.println("There is no " + value);
-                break;
             }
+        } else {
+            System.out.println("There is no " + value);
         }
     }
 }
