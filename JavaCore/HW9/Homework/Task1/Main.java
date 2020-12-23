f3a7204a6fbd72f212ff2e2ea5f34348ec7180c3
Andrew Kobryn <andrzjha@gmail.com>
2020-12-24 00:19:28 +0200

    add some code to HW9/Task1

--- JavaCore/HW9/Homework/Task1/Main.java
+++ JavaCore/HW9/Homework/Task1/Main.java
@@ -1,25 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.ArrayList;
+import java.util.List;
+//Using this method write a method main(), that has to enter 10 numbers:
+//	a1, a2, ..., a10, such that 1 < a1 < ... < a10 < 100
+//Не розібрався всеодно(
 public class Main {
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         double numberA;
         double numberB;
+        List<Integer> list = new ArrayList<>(10);
         try {
-            System.out.println("Enter first number: ");
+            System.out.print("Enter first number: ");
             numberA = Double.parseDouble(bufferedReader.readLine());
-            System.out.println("Enter second number: ");
+            System.out.print("Enter second number: ");
             numberB = Double.parseDouble(bufferedReader.readLine());
             div(numberA, numberB);
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.print("Try your lucky (tip: try to enter int number): ");
+            readNumber(10, 1, bufferedReader);
+
+        } catch (IOException | OutOfIntRange e) {
+            System.err.println(e.getMessage());
         } catch (NumberFormatException e) {
-            System.err.println("Oops, please enter a number");
+            System.err.println("Oops, enter the desired data type");
         }
-        System.out.println("Like us?");
     }
+
     //не получилось в мене щоб метод повертав тип double
     //catch не ловив помилку ділення на 0 і результат завжди виводився Infinity
     //Мені не дуже подобається моє рішення, але поки так
@@ -30,4 +38,27 @@ public class Main {
             System.out.println(a / b);
         }
     }
+    //метод для визначення чи знаходиться число у вказаному діапазоні
+    public static void readNumber(int start, int end, BufferedReader bufferedReader) throws IOException, OutOfIntRange {
+        int value = Integer.parseInt(bufferedReader.readLine());
+        if (start > end) {
+            int temp = start;
+            start = end;
+            end = temp;
+        }
+        if (start <= value && value <= end) {
+            System.out.println("Congratulations! Your answer " + value + " is in the range!");
+        } else {
+            throw new OutOfIntRange("Bad day(");
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
---
+++ JavaCore/HW9/Homework/Task1/OutOfIntRange.java
@@ -0,0 +1,5 @@
+public class OutOfIntRange extends Exception{
+    public OutOfIntRange(String arg0){
+        super(arg0);
+    }
+}
