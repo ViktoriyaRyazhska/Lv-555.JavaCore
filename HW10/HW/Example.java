5176923cf6913d5356ba7b698113e425103ace0d
NickolayGryzov <nickolay.gryzov@gmail.com>
2021-01-05 13:18:18 +0200

    HW10

--- HW10/HW/Example.java
+++ HW10/HW/Example.java
@@ -1,4 +1,7 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Example {
 	public static void main(String... var)
@@ -16,6 +19,7 @@ public class Example {
 		System.out.println(list2);
 		System.out.println(encrypt("abc", 3));
 		System.out.println(decrypt("def", 3));
+		checkFormat("131.13.12");
 	}
 	public static String encrypt(String s, int n) {
 		char[] chars = s.toCharArray();
@@ -32,5 +36,19 @@ public class Example {
 		}
 		s = String.valueOf(chars);
 		return s;
+	}
+	public static void checkFormat(String s) {
+		SimpleDateFormat form = new SimpleDateFormat("mm.dd.yy");
+		Date d;
+		try {
+			d = form.parse(s);
+			if (form.format(d).equals(s)) {
+				System.out.println("Valid");
+			}else {
+				System.out.println("Invalid");
+			}
+		} catch (ParseException e) {
+			System.out.println("Invalid");
+		}
 	}
 }
