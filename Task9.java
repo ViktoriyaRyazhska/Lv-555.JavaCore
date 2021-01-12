9cc03e63e145728a365dd3562fc93fb3fcb3cae7
DTomSawyer <75069848+DTomSawyer@users.noreply.github.com>
2021-01-12 18:57:49 +0200

    Add files via upload

--- Task9.java
+++ Task9.java
@@ -1,24 +1,24 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Task9 {
-	public void execute() {
+    public static void execute() {
 
-		System.out.println(counter(10));
-	}
-
-	public static int randomMethod() {
-		Random random = new Random();
-		return random.nextInt(2);
-	}
-
-	public static int counter(int n) {
-		int counter = 0;
-		for (int i = 0; i <= n; i++) {
-			int z = randomMethod();
-			if (z == 1) {
-				counter++;
-			}
-		}
-		return counter;
-	}
+        System.out.println(counter(10));
+    }
+    public static int randomMethod() {
+        Random random = new Random();
+        return random.nextInt(2);
+    }
+    public static int counter() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int counter = 0;
+        for (int i = 0; i <= n; i++) {
+            int z = randomMethod();
+            if (z == 1) {
+                counter++;
+            }
+        }return counter;
+    }
 }
