0fda194cc586d5925ce96f68037331166998c6ef
DTomSawyer <75069848+DTomSawyer@users.noreply.github.com>
2021-01-12 18:42:28 +0200

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task1 {
+
+        public void execute (){
+            System.out.println((fibonacci(10)));
+    }
+
+    public static int[] fibonacci(int n) {
+        int[] fibonacci = new int[n];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]);
+
+        }
+        return fibonacci;
+    }
+    public static void fib(int[] n){
+        for(int i = 0; i < n.length; ++i ){
+            System.out.println(n[i]);
+        }
+    }
+}
