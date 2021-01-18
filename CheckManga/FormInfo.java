28055e6a5e94f10a611fb70a4010c0a001579660
NickolayGryzov <nickolay.gryzov@gmail.com>
2021-01-18 08:07:15 +0200

    Add color select

--- CheckManga/FormInfo.java
+++ CheckManga/FormInfo.java
@@ -3,10 +3,15 @@ import java.io.IOException;
 public class FormInfo {
 	public static String formInfo() throws IOException {
 		String[][] results = ReadSettings.readSettings();
-		String result = "";
+		String result = "<html>";
+		String currentChapter = "";
 		for (int i = 0; i < results.length; i++) {
-			result+=results[i][0] + ": "+CheckURL.checkURL(results[i][1])+" was: "+results[i][2]+"\n";
+			currentChapter = CheckURL.checkURL(results[i][1]);
+			if (!currentChapter.equals(results[i][2]))
+				currentChapter="<font color='red'>"+currentChapter+"</font>";
+			result+=results[i][0] + ": "+currentChapter+" was: "+results[i][2]+"<br>";
 		}
+		result+="</html>";
 		return result;
 	}
 }
